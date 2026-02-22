0fce37ad6d03db5c4ff5a29b5307359cabe3f806
EV4 Porter <ev4@example.com>
2026-02-22 18:16:21 +0900

    fix(ev4): bypass counter validation for incoming messages
    - Renamed COUNTER/CHECKSUM to COUNTER_T/CHECKSUM_T for incoming messages to restore canValid
    - Kept standard names for outgoing control messages (LFA, SCC_CONTROL, LKAS_ALT, ADRV)
    - Updated carstate.py to read renamed COUNTER_T for buttons

--- opendbc/car/hyundai/carstate.py
+++ opendbc/car/hyundai/carstate.py
@@ -291,7 +291,7 @@ class CarState(CarStateBase):
     self.cruise_buttons.extend(cp.vl_all[self.cruise_btns_msg_canfd]["CRUISE_BUTTONS"])
     self.main_buttons.extend(cp.vl_all[self.cruise_btns_msg_canfd]["ADAPTIVE_CRUISE_MAIN_BTN"])
     self.lda_button = cp.vl[self.cruise_btns_msg_canfd]["LDA_BTN"]
-    self.buttons_counter = cp.vl[self.cruise_btns_msg_canfd].get("COUNTER", 0)
+    self.buttons_counter = cp.vl[self.cruise_btns_msg_canfd].get("COUNTER", cp.vl[self.cruise_btns_msg_canfd].get("COUNTER_T", 0))
     ret.accFaulted = cp.vl["TCS"]["ACCEnable"] != 0  # 0 ACC CONTROL ENABLED, 1-3 ACC CONTROL DISABLED
 
     if self.CP.flags & HyundaiFlags.CANFD_LKA_STEERING:
