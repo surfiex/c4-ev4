70f9065a9c4c0eeca64678e9f75f9c7da40b0e5d
EV4 Porter <ev4@example.com>
2026-03-10 12:46:34 +0900

    ev4: prune redundant vehicle-origin IDs from forward_ids to resolve bus collisions

--- opendbc/car/hyundai/carstate.py
+++ opendbc/car/hyundai/carstate.py
@@ -334,12 +334,11 @@ class CarState(CarStateBase):
 
     # Include all ADRV/HDA2 IDs found on Bus 2 (Camera Bus) that need to be forwarded to Bus 1 (ECAN)
     # PRUNED: Removed ONLY IDs spoofed in hyundaicanfd.py:create_adrv_messages_ev4 (81, 352, 474, 490, 512, 811, 813, 816, 837)
-    forward_ids = [256, 282, 298, 357, 416, 437, 480, 506, 698, 752, 864, 865, 866, 867, 868, 896, 905, 917, 928, 976, 977, 978, 979, 980, 1041, 1280] + \
+    forward_ids = [282, 298, 357, 416, 437, 480, 506, 698, 752, 866, 867, 868, 896, 905, 917, 928] + \
                   list(range(560, 585)) + list(range(933, 965))
     for addr in forward_ids:
       msg_name = None
-      if addr == 256: msg_name = "ACCELERATOR_BRAKE_ALT"
-      elif addr == 282: msg_name = "FR_CMR_01_10ms"
+      if addr == 282: msg_name = "FR_CMR_01_10ms"
       elif addr == 298: msg_name = "LFA"
       elif addr == 357: msg_name = "ADRV_0x165"
       elif addr == 416: msg_name = "SCC_CONTROL"
@@ -348,8 +347,6 @@ class CarState(CarStateBase):
       elif addr == 506: msg_name = "ISLA"
       elif addr == 698: msg_name = "IFS_0x2ba"
       elif addr == 752: msg_name = "ID752"
-      elif addr == 864: msg_name = "LFA_BUTTON"
-      elif addr == 865: msg_name = "ID865"
       elif addr == 866: msg_name = "CAM_0x362"
       elif addr == 867: msg_name = "CAM_0x363"
       elif addr == 868: msg_name = "CAM_0x364"
@@ -357,13 +354,6 @@ class CarState(CarStateBase):
       elif addr == 905: msg_name = "ADRV_0x389"
       elif addr == 917: msg_name = "ID917"
       elif addr == 928: msg_name = "ID928"
-      elif addr == 1041: msg_name = "DOORS_SEATBELTS"
-      elif addr == 976: msg_name = "EV4_BODY_1"
-      elif addr == 977: msg_name = "ID977"
-      elif addr == 978: msg_name = "ID978"
-      elif addr == 979: msg_name = "EV4_BODY_2"
-      elif addr == 980: msg_name = "ID980"
-      elif addr == 1280: msg_name = "ID1280"
       elif 560 <= addr <= 584:
         msg_name = "RADAR_0x240" if addr == 576 else f"HBA_0x{addr:03x}"
       elif 933 <= addr <= 964: msg_name = f"RADAR_TRACK_{addr}"
